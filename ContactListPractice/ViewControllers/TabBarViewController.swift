672084839e2139179c3fa6179bbf27b2e5bd4480
Artem Potemkin (M1 Pro) <art.potemkin@gmail.com>
2023-07-07 12:30:48 +0300

    corrections after checking homework 1

--- ContactListPractice/ViewControllers/TabBarViewController.swift
+++ ContactListPractice/ViewControllers/TabBarViewController.swift
@@ -8,23 +8,35 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getContacts()
     }
-
+    
     private func getContacts() {
         let contacts = Person.getPersons()
         
-        guard let navigationContactListVC = viewControllers?.first as? UINavigationController else { return }
-        guard let contactListVC = navigationContactListVC.topViewController as? ContactListViewController else { return }
-        
-        contactListVC.contacts = contacts
-        
-        guard let navigationPersonsListVC = viewControllers?.last as? UINavigationController else { return }
-        guard let personsListVC = navigationPersonsListVC.topViewController as? PersonsListViewController else { return }
-        
-        personsListVC.contacts = contacts
+        viewControllers?.forEach({ viewController in
+            guard let navigatContrlr = viewController as? UINavigationController else { return }
+            
+            if let contactListVC = navigatContrlr.topViewController as? ContactListViewController {
+                contactListVC.contacts = contacts
+                
+            } else if let personsListVC = navigatContrlr.topViewController as? PersonsListViewController {
+                personsListVC.contacts = contacts
+            }
+        })
     }
 }
+
+// мой старый вариант
+//guard let navigationContactListVC = viewControllers?.first as? UINavigationController else { return }
+//guard let contactListVC = navigationContactListVC.topViewController as? ContactListViewController else { return }
+//
+//contactListVC.contacts = contacts
+//
+//guard let navigationPersonsListVC = viewControllers?.last as? UINavigationController else { return }
+//guard let personsListVC = navigationPersonsListVC.topViewController as? PersonsListViewController else { return }
+//
+//personsListVC.contacts = contacts
